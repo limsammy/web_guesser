8a0fd78ba247ba95e1908252eef82b23cb0789f4
Sam Lim <limsammy1@gmail.com>
2017-04-23 18:58:22 -0700

    created class for NumGen, with reader secret_number that gets instantiated outside the get '/' so it is static on refresh, not regenning each time

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+class NumGen
+  attr_reader :secret_number
+
+  def initialize
+    @secret_number = rand(100)
+  end
+end
+
+number = NumGen.new.secret_number
 get '/' do
-  number = rand(100)
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  erb :index, :locals => {:number => number, :guess => guess}
   # throw params.inspect
   # params["guess"]
+
+  # if the guess is too high
+  #   set the message to "Too high!"
+  # end
+  # render the ERB template and pass in the number AND the message
+
 end
